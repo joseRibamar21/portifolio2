d9f68f97c7ac6b5ef6c95da0caa913115df47d10
joseRibamar21 <jose.21rb@outlook.com>
2022-12-27 11:58:18 -0300

    teste

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -27,7 +27,7 @@ export default createGlobalStyle`
   }
 
   body{
-    @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,800;0,900;1,700&display=swap');
+    /* @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,800;0,900;1,700&display=swap'); */
     background: #000610;
     color: #fff;
   }
